6dbdbe6b0b80c6933ceb9218aacb6dc4c0493569
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-06-02 20:47:43 -0500

    Provided a way to specify the state's user property name.
    Fixes #1.

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ const getTokenFromHeader = (header, cookie) => {
 };
 
 const getUser = (options, pools) => {
-  const opt = {authHeader: 'Authorization', authCookie: 'auth', region: '', userPoolId: '', setAuthCookieOptions: null};
-  options && Object.assign(opt, options);
+  const opt = {authHeader: 'Authorization', authCookie: 'auth', region: '', userPoolId: '', setAuthCookieOptions: null, ...options};
   if (typeof opt.source != 'function') {
     opt.source = getTokenFromHeader(opt.authHeader, opt.authCookie);
   }
-  const getUser = makeGetUser(pools || opt);
+  const getRawUser = makeGetUser(pools || opt);
   const setAuthCookie = (ctx, cookieOptions) => {
-    if (ctx.state.user && opt.authCookie && ctx.cookies.get(opt.authCookie) !== ctx.state.user._token) {
-      ctx.cookies.set(
-        opt.authCookie,
-        ctx.state.user._token,
-        Object.assign({expires: new Date(ctx.state.user.exp * 1000), domain: ctx.host, overwrite: true}, cookieOptions)
-      );
+    if (ctx.state[getUser.stateUserProperty] && opt.authCookie && ctx.cookies.get(opt.authCookie) !== ctx.state[getUser.stateUserProperty]._token) {
+      ctx.cookies.set(opt.authCookie, ctx.state[getUser.stateUserProperty]._token, {
+        expires: new Date(ctx.state[getUser.stateUserProperty].exp * 1000),
+        domain: ctx.host,
+        overwrite: true,
+        ...cookieOptions
+      });
     }
   };
   return async (ctx, next) => {
     const token = opt.source(ctx);
-    const user = await getUser(token);
+    const user = await getRawUser(token);
     if (user) {
       user._token = token;
       user.setAuthCookie = setAuthCookie;
     }
-    ctx.state.user = user;
+    ctx.state[getUser.stateUserProperty] = user;
     await next();
     if (opt.setAuthCookieOptions && user) user.setAuthCookie(ctx, opt.setAuthCookieOptions);
   };
 };
 
 const isAuthenticated = async (ctx, next) => {
-  if (ctx.state.user) return next();
+  if (ctx.state[getUser.stateUserProperty]) return next();
   ctx.status = 401;
 };
 
 const hasGroup = group => async (ctx, next) => {
-  if (ctx.state.user) {
-    const groups = ctx.state.user['cognito:groups'];
+  if (ctx.state[getUser.stateUserProperty]) {
+    const groups = ctx.state[getUser.stateUserProperty]['cognito:groups'];
     if (groups && groups instanceof Array && groups.some(g => g === group)) return next();
     ctx.status = 403;
   }
@@ -53,8 +53,8 @@ const hasGroup = group => async (ctx, next) => {
 };
 
 const hasScope = scope => async (ctx, next) => {
-  if (ctx.state.user) {
-    const scopes = ctx.state.user.scope;
+  if (ctx.state[getUser.stateUserProperty]) {
+    const scopes = ctx.state[getUser.stateUserProperty].scope;
     if (scopes && typeof scopes == 'string' && scope.split(' ').some(s => s === scope)) return next();
     ctx.status = 403;
   }
@@ -62,13 +62,15 @@ const hasScope = scope => async (ctx, next) => {
 };
 
 const isAllowed = validator => async (ctx, next) => {
-  const scopes = (ctx.state.user && ctx.state.user.scope && ctx.state.user.scope.split(' ')) || [],
-    groups = (ctx.state.user && ctx.state.user['cognito:groups']) || [];
+  const scopes =
+      (ctx.state[getUser.stateUserProperty] && ctx.state[getUser.stateUserProperty].scope && ctx.state[getUser.stateUserProperty].scope.split(' ')) || [],
+    groups = (ctx.state[getUser.stateUserProperty] && ctx.state[getUser.stateUserProperty]['cognito:groups']) || [];
   const pass = await validator(ctx, groups, scopes);
   if (pass) return next();
-  ctx.status = ctx.state.user ? 403 : 401;
+  ctx.status = ctx.state[getUser.stateUserProperty] ? 403 : 401;
 };
 
+getUser.stateUserProperty = 'user';
 getUser.isAuthenticated = isAuthenticated;
 getUser.hasGroup = hasGroup;
 getUser.hasScope = hasScope;
